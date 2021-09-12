94de78994dcde46af51909f11ca43df8e5370f86
yakitori99 <t2.akg3@gmail.com>
2021-09-12 21:50:11 +0900

    configの設定値を変更

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 // 複数ファイルから利用される共通の設定値
 export default Object.freeze({
-  API_BASE_URL:'http://localhost:1323',
-  // API_BASE_URL:'https://trip-weather-backend.herokuapp.com',
+  // API_BASE_URL:'http://localhost:1323',
+  API_BASE_URL:'https://trip-weather-backend.herokuapp.com',
 
   // ニックネームなしの場合の表示文字列
   noNickname:"（ニックネームなし）",
